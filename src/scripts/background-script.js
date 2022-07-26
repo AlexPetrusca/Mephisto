555cd9cc2a2be0a8048e16b283a1861d2e384c4f
AlexPetrusca <petruscalex@gmail.com>
2022-07-26 10:09:04 -0700

    remove backgrounds script

--- src/scripts/background-script.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function (msg, sender) {
-  if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-    chrome.pageAction.show(sender.tab.id);
-  }
-});
-
-
