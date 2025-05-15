321747a7692297d5deca9679b7492fe97e979041
Dev <petruscalex@gmail.com>
2025-05-14 18:25:39 -0700

    change settings without hitting apply

--- src/options/util/SettingsPage.js
+++ src/options/util/SettingsPage.js
@@ -2,16 +2,13 @@ import { FormElement } from "./FormElement.js";
 
 export class SettingsPage {
     resetButton;
-    applyButton;
     formElements;
-    configUniqueifier;
 
     constructor() {
         if (this.constructor === SettingsPage) {
             throw new Error("Can't instantiate abstract class!");
         }
         this.formElements = [];
-        this.configUniqueifier = '';
     }
 
     init() {
@@ -19,46 +16,33 @@ export class SettingsPage {
     }
 
     onInit() {
-        this.applyButton = document.getElementById('apply_btn');
-        this.applyButton.addEventListener('click', () => this.onApplyConfigValues());
         this.resetButton = document.getElementById('reset_btn');
         this.resetButton.addEventListener('click', () => this.onResetConfigValues());
 
         this.init();
-
         this.pullConfigValues();
-        this.onConfigValuesChanged();
-    }
-
-    // form uniqueifier
-    createUniquifier() {
-        return this.formElements.reduce((acc, formElement) => {
-            return acc + formElement.name + ':' + formElement.getValue() + ' ';
-        }, '');
-    }
-
-    updateUniquifier() {
-        this.configUniqueifier = this.createUniquifier();
     }
 
     clearConfigValues() {
         this.formElements.forEach(formElement => {
             localStorage.removeItem(formElement.name);
         });
-        this.updateUniquifier();
     }
 
     // localstorage values push/pull
     pullConfigValues() {
+        console.log(JSON.parse(JSON.stringify(localStorage)))
         this.formElements.forEach(formElement => {
             const localStorageVal = localStorage.getItem(formElement.name);
+            console.log(localStorageVal);
             if (localStorageVal) {
+                console.log("FOUND:", formElement.name, '-->', JSON.parse(localStorageVal));
                 formElement.setValue(JSON.parse(localStorageVal));
             } else {
+                console.log("MISS:", formElement.name, '-->', formElement.default);
                 formElement.setValue(formElement.default);
             }
         });
-        this.updateUniquifier();
     }
 
     pushConfigValues() {
@@ -68,30 +52,24 @@ export class SettingsPage {
                 : formElement.getValue();
             localStorage.setItem(formElement.name, formValue);
         });
-        this.updateUniquifier();
     }
 
     // register form element
     registerFormElement(name, description, type, defaultValue) {
         const formElement = new FormElement(name, description, type, defaultValue);
-        formElement.registerChangeListener(() => this.onConfigValuesChanged());
+        formElement.registerChangeListener(() => {
+            const formValue = (formElement.valueType === 'string')
+                ? `"${formElement.getValue()}"`
+                : formElement.getValue();
+            localStorage.setItem(formElement.name, formValue);
+        });
         this.formElements.push(formElement);
         return formElement;
     }
 
     // on event callbacks
-    onApplyConfigValues() {
-        this.pushConfigValues();
-        this.onConfigValuesChanged();
-    }
-
     onResetConfigValues() {
         this.clearConfigValues();
         this.pullConfigValues();
-        this.onConfigValuesChanged();
-    }
-
-    onConfigValuesChanged() {
-        this.applyButton.disabled = (this.configUniqueifier === this.createUniquifier());
     }
 }
